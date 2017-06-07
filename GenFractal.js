066266bbc9de7a70c8846b6c850019308203e6b8
mrinal123456789 <mrinal.thomas@icsd.k12.ny.us>
2017-06-06 22:41:38 -0400

    Dynamics

--- GenFractal.js
+++ GenFractal.js
@@ -5,18 +5,27 @@ var r_c1 = [255,0,0];
 var r_c2 = [0,150,0];
 var r_c3 = [0,0,255];
 
+var rIterCount = 0;
+var gIterCount = 0;
+var bIterCount = 0;
+var nIterCount = 0;
+var rCount = 0;
+var gCount = 0;
+var bCount = 0;
+var nCount = 0;
+
 function newton(p, c)
 {
-	var num = multiply(subtract(p, subtract({"x":-0.5,"y":0}, c)), multiply(subtract(p, {"x":1,"y":0}),subtract(p, add({"x":-0.5,"y":0}, c))));
-	var denom = subtract(subtract(scalar(multiply(p,p),3),{"x":0.75,"y":0}), multiply(c,c));
+	let a = subtract({"x":-0.5,"y":0}, c);
+	let b = add({"x":-0.5,"y":0}, c);
+	let pa = subtract(p, a);
+	let pb = subtract(p, b);
+	let p1 = subtract(p, {"x":1,"y":0});
+	let p1xpb = multiply(p1,pb);
+	var num = multiply(pa, p1xpb);
+	var denom = add(add(multiply(pa, pb), p1xpb), multiply(p1, pa));
+	//var denom = subtract(subtract(scalar(multiply(p,p),3),{"x":0.75,"y":0}), multiply(c,c));
 	return subtract(p, divide(num, denom));
-	
-	//let a = add({"x":-0.5,"y":0}, c);
-	//let b = subtract({"x":-0.5,"y":0}, c);
-	//var num = multiply(subtract(p, {"x":1,"y":0}), multiply(subtract(p,a),subtract(p,b)));
-	//var denom = add(multiply(subtract(p,a),subtract(p,b)), add(multiply(subtract(p, {"x":1,"y":0}),subtract(p,b)),multiply(subtract(p, {"x":1,"y":0}),subtract(p,a))))
-	
-	//return subtract(p, divide(subtract(multiply(p,multiply(p,p)),{"x":1,"y":0}),scalar(multiply(p,p),3)))
 }
 
 function multiply(p1, p2)
@@ -72,8 +81,15 @@ onmessage = function(msg)
 		postMessage({id: d.id, data: genFractalL(d.data[0],d.data[1],d.data[2],d.data[3],d.data[4],d.data[5],d.data[6],d.data[7])});
 	else if (d.id == 20)
 		postMessage({id: d.id, data: genFractalR(d.data[0],d.data[1],d.data[2],d.data[3],d.data[4],d.data[5],d.data[6],d.data[7])});
+	else if (d.id == 30)
+		postMessage({id: d.id, data: genDynamicL(d.data[0],d.data[1],d.data[2],d.data[3],d.data[4],d.data[5],d.data[6],d.data[7],d.data[8])});
+	else if (d.id == 40)
+		postMessage({id: d.id, data: genDynamicR(d.data[0],d.data[1],d.data[2],d.data[3],d.data[4],d.data[5],d.data[6],d.data[7],d.data[8])});
+	
 	close();
 }
+
+
 
 function genFractalL(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 {
@@ -138,31 +154,31 @@ function genFractalL(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 					imgData.data[rPos + 3] = 255;
 					found = true;
 				}
-				else if (iteration > min_res && distanceSqr(p0, p1) < 0.0001)
+				else if (iteration > min_res && distanceSqr(p0, p1) < 0.000005)
 				{
 					var d1 = Math.sqrt(distanceSqr(p0, r_1));
-					if (Math.abs(d1) < 0.00005)
+					if (Math.abs(d1) < 0.00001)
 					{
 						//console.log(d1)
-						imgData.data[rPos] = r_c1[0]
+						imgData.data[rPos] = r_c1[0];
 						imgData.data[rPos + 1] = r_c1[1];
 						imgData.data[rPos + 2] = r_c1[2];
 						imgData.data[rPos + 3] = 100 + 130 * (iteration - min_res) / (max_res - min_res);
 						found = true;
 					}
 					var d2 = distanceSqr(p0, r_2);
-					if (Math.abs(d2) < 0.00005)
+					if (Math.abs(d2) < 0.00001)
 					{
-						imgData.data[rPos] = r_c2[0]
+						imgData.data[rPos] = r_c2[0];
 						imgData.data[rPos + 1] = r_c2[1];
 						imgData.data[rPos + 2] = r_c2[2];
 						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
 						found = true;
 					}
 					var d3 = distanceSqr(p0, r_3);
-					if (Math.abs(d3) < 0.00005)
+					if (Math.abs(d3) < 0.00001)
 					{
-						imgData.data[rPos] = r_c3[0]
+						imgData.data[rPos] = r_c3[0];
 						imgData.data[rPos + 1] = r_c3[1];
 						imgData.data[rPos + 2] = r_c3[2];
 						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
@@ -176,6 +192,15 @@ function genFractalL(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 			//	postMessage({data:imgData});
 		}
 	}
+	console.log(rCount);
+	console.log(gCount);
+	console.log(bCount);
+	console.log(nCount);
+	console.log(rIterCount);
+	console.log(gIterCount);
+	console.log(bIterCount);
+	console.log(nIterCount);
+	
 	return imgData;
 }
 
@@ -184,7 +209,7 @@ function genFractalR(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 {
 	var X_STEP = 2 * w_w / c_w;
 	var Y_STEP = 2 * w_h / c_h
-	var min_res = Math.floor(max_res * 0.1);
+	var min_res = 0;//Math.floor(max_res * 0.1);
 	for (var x = c_w - 1; x >= c_w / 2; x--)
 	{
 		for (var y = 0; y < c_h;y++)
@@ -241,11 +266,13 @@ function genFractalR(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 					imgData.data[rPos + 2] = 42;
 					imgData.data[rPos + 3] = 255;
 					found = true;
+					nCount++;
+					nIterCount += iteration;
 				}
-				else if (iteration > min_res && distanceSqr(p0, p1) < 0.0001)
+				else if (iteration > min_res && distanceSqr(p0, p1) < 0.000005)
 				{
 					var d1 = Math.sqrt(distanceSqr(p0, r_1));
-					if (Math.abs(d1) < 0.00005)
+					if (Math.abs(d1) < 0.00001)
 					{
 						//console.log(d1)
 						imgData.data[rPos] = r_c1[0]
@@ -253,24 +280,30 @@ function genFractalR(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 						imgData.data[rPos + 2] = r_c1[2];
 						imgData.data[rPos + 3] = 100 + 130 * (iteration - min_res) / (max_res - min_res);
 						found = true;
+						rCount++;
+						rIterCount += iteration;
 					}
 					var d2 = distanceSqr(p0, r_2);
-					if (Math.abs(d2) < 0.00005)
+					if (Math.abs(d2) < 0.00001)
 					{
 						imgData.data[rPos] = r_c2[0]
 						imgData.data[rPos + 1] = r_c2[1];
 						imgData.data[rPos + 2] = r_c2[2];
 						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
 						found = true;
+						gCount++;
+						gIterCount += iteration;
 					}
 					var d3 = distanceSqr(p0, r_3);
-					if (Math.abs(d3) < 0.00005)
+					if (Math.abs(d3) < 0.00001)
 					{
 						imgData.data[rPos] = r_c3[0]
 						imgData.data[rPos + 1] = r_c3[1];
 						imgData.data[rPos + 2] = r_c3[2];
 						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
 						found = true;
+						bCount++;
+						bIterCount += iteration;
 					}
 				}
 				iteration++;
@@ -279,5 +312,233 @@ function genFractalR(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData)
 			//	postMessage({data:imgData});
 		}
 	}
+	console.log(rCount);
+	console.log(gCount);
+	console.log(bCount);
+	console.log(nCount);
+	console.log(rIterCount);
+	console.log(gIterCount);
+	console.log(bIterCount);
+	console.log(nIterCount);
+	return imgData;
+}
+
+function genDynamicL(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData, c)
+{
+	var X_STEP = 2 * w_w / c_w;
+	var Y_STEP = 2 * w_h / c_h
+	var min_res = 0;//Math.floor(max_res * 0.1);
+	
+	for (var x = 0; x < c_w / 2; x++)//(var x = 0; x < c_w / 2; x++)
+	{
+		for (var y = 0; y < c_h; y++)//(var y = 0; y < c_h;y++)
+		{
+			var rPos = x * 4 + y * 4 * (c_w / 2);
+			//imgData.data[rPos] = 255;
+			//imgData.data[rPos + 1] = 0;
+			//imgData.data[rPos + 2] = 0;
+			//imgData.data[rPos + 3] = 255;
+			var coordX = w_x - w_w + x * X_STEP;
+			var coordY = w_y - w_h + y * Y_STEP;
+			var r_2 = add({"x":-0.5,"y":0}, c);
+			var r_3 = subtract({"x":-0.5,"y":0}, c);
+			var p0 = new Array();
+			p0.x = coordX;
+			p0.y = coordY;
+			var iteration = 0;
+			found = false;
+			while (!found)
+			{
+				var p1 = p0;
+				p0 = newton(p0, c);
+				if (iteration > max_res)
+				{
+					/*var d1 = distanceSqr(p0, r_1);
+					var d2 = distanceSqr(p0, r_2);
+					var d3 = distanceSqr(p0, r_3);
+					if (d1 < d2 && d1 < d3)
+					{
+						imgData.data[rPos] = r_c1[0]
+						imgData.data[rPos + 1] = r_c1[1];
+						imgData.data[rPos + 2] = r_c1[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+					}
+					else if (d2 < d1 && d2 < d3)
+					{
+						imgData.data[rPos] = r_c2[0]
+						imgData.data[rPos + 1] = r_c2[1];
+						imgData.data[rPos + 2] = r_c2[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+					}
+					else
+					{
+						imgData.data[rPos] = r_c3[0]
+						imgData.data[rPos + 1] = r_c3[1];
+						imgData.data[rPos + 2] = r_c3[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+					}*/
+					imgData.data[rPos] = 34;
+					imgData.data[rPos + 1] = 41;
+					imgData.data[rPos + 2] = 42;
+					imgData.data[rPos + 3] = 255;
+					found = true;
+				}
+				else if (iteration > min_res && distanceSqr(p0, p1) < 0.000005)
+				{
+					var d1 = Math.sqrt(distanceSqr(p0, r_1));
+					if (Math.abs(d1) < 0.000001)
+					{
+						//console.log(d1)
+						imgData.data[rPos] = r_c1[0];
+						imgData.data[rPos + 1] = r_c1[1];
+						imgData.data[rPos + 2] = r_c1[2];
+						imgData.data[rPos + 3] = 100 + 130 * (iteration - min_res) / (max_res - min_res);
+						found = true;
+					}
+					var d2 = distanceSqr(p0, r_2);
+					if (Math.abs(d2) < 0.000001)
+					{
+						imgData.data[rPos] = r_c2[0];
+						imgData.data[rPos + 1] = r_c2[1];
+						imgData.data[rPos + 2] = r_c2[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+						found = true;
+					}
+					var d3 = distanceSqr(p0, r_3);
+					if (Math.abs(d3) < 0.000001)
+					{
+						imgData.data[rPos] = r_c3[0];
+						imgData.data[rPos + 1] = r_c3[1];
+						imgData.data[rPos + 2] = r_c3[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+						found = true;
+					}
+				}
+				iteration++;
+			}
+			//console.log(p0)
+			//if (x % 10 == 0 && y == c_h - 1)
+			//	postMessage({data:imgData});
+		}
+	}
+	console.log(rCount);
+	console.log(gCount);
+	console.log(bCount);
+	console.log(nCount);
+	console.log(rIterCount);
+	console.log(gIterCount);
+	console.log(bIterCount);
+	console.log(nIterCount);
+	
+	return imgData;
+}
+
+function genDynamicR(c_w, c_h, w_x, w_y, w_w, w_h, max_res, imgData, c)
+{
+	var X_STEP = 2 * w_w / c_w;
+	var Y_STEP = 2 * w_h / c_h
+	var min_res = 0;//Math.floor(max_res * 0.1);
+	
+	for (var x = c_w - 1; x >= c_w / 2; x--)
+	{
+		for (var y = 0; y < c_h;y++)
+		{
+			var rPos = x * 4 + y * 4 * (c_w / 2);
+			//imgData.data[rPos] = 255;
+			//imgData.data[rPos + 1] = 0;
+			//imgData.data[rPos + 2] = 0;
+			//imgData.data[rPos + 3] = 255;
+			var coordX = w_x - w_w + x * X_STEP;
+			var coordY = w_y - w_h + y * Y_STEP;
+			var r_2 = add({"x":-0.5,"y":0}, c);
+			var r_3 = subtract({"x":-0.5,"y":0}, c);
+			var p0 = new Array();
+			p0.x = coordX;
+			p0.y = coordY;
+			var iteration = 0;
+			found = false;
+			while (!found)
+			{
+				var p1 = p0;
+				p0 = newton(p0, c);
+				if (iteration > max_res)
+				{
+					/*var d1 = distanceSqr(p0, r_1);
+					var d2 = distanceSqr(p0, r_2);
+					var d3 = distanceSqr(p0, r_3);
+					if (d1 < d2 && d1 < d3)
+					{
+						imgData.data[rPos] = r_c1[0]
+						imgData.data[rPos + 1] = r_c1[1];
+						imgData.data[rPos + 2] = r_c1[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+					}
+					else if (d2 < d1 && d2 < d3)
+					{
+						imgData.data[rPos] = r_c2[0]
+						imgData.data[rPos + 1] = r_c2[1];
+						imgData.data[rPos + 2] = r_c2[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+					}
+					else
+					{
+						imgData.data[rPos] = r_c3[0]
+						imgData.data[rPos + 1] = r_c3[1];
+						imgData.data[rPos + 2] = r_c3[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+					}*/
+					imgData.data[rPos] = 34;
+					imgData.data[rPos + 1] = 41;
+					imgData.data[rPos + 2] = 42;
+					imgData.data[rPos + 3] = 255;
+					found = true;
+				}
+				else if (iteration > min_res && distanceSqr(p0, p1) < 0.000005)
+				{
+					var d1 = Math.sqrt(distanceSqr(p0, r_1));
+					if (Math.abs(d1) < 0.00001)
+					{
+						//console.log(d1)
+						imgData.data[rPos] = r_c1[0];
+						imgData.data[rPos + 1] = r_c1[1];
+						imgData.data[rPos + 2] = r_c1[2];
+						imgData.data[rPos + 3] = 100 + 130 * (iteration - min_res) / (max_res - min_res);
+						found = true;
+					}
+					var d2 = distanceSqr(p0, r_2);
+					if (Math.abs(d2) < 0.00001)
+					{
+						imgData.data[rPos] = r_c2[0];
+						imgData.data[rPos + 1] = r_c2[1];
+						imgData.data[rPos + 2] = r_c2[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+						found = true;
+					}
+					var d3 = distanceSqr(p0, r_3);
+					if (Math.abs(d3) < 0.00001)
+					{
+						imgData.data[rPos] = r_c3[0];
+						imgData.data[rPos + 1] = r_c3[1];
+						imgData.data[rPos + 2] = r_c3[2];
+						imgData.data[rPos + 3] = 100 +  130 * (iteration - min_res) / (max_res - min_res);
+						found = true;
+					}
+				}
+				iteration++;
+			}
+			//console.log(p0)
+			//if (x % 10 == 0 && y == c_h - 1)
+			//	postMessage({data:imgData});
+		}
+	}
+	console.log(rCount);
+	console.log(gCount);
+	console.log(bCount);
+	console.log(nCount);
+	console.log(rIterCount);
+	console.log(gIterCount);
+	console.log(bIterCount);
+	console.log(nIterCount);
+	
 	return imgData;
 }
